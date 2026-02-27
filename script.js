d1a1d017d976b847c93b87a17fff7324e8c2d261
Gabv05 <gabv2005@gmail.com>
2026-02-27 11:45:25 +0000

    final push

--- script.js
+++ script.js
@@ -1,81 +1,235 @@
-let scene, camera, renderer, box, clock, mixer, action = [], mode;
+var scene, camera, renderer, clock, mixer, actions = [], mode, isWireFrame = false, params, lights;
+let loadedModel;
+let secondModelMixer, secondModelActions = [];
+let sound, secondSound;
 
 init();
 
 function init() {
-    const assetPath = './';
+    const assetPath = './'; // Path to assets
 
     clock = new THREE.Clock();
 
+    // Create the scene
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0x00aaff);
 
+    // Set up the camera
     camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set = (-5, 25, 20);
+    camera.position.set(0, 10, 10);
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-    document.body.appendChild(renderer.domElement);
-
+    // Add lighting
     const ambient = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
     scene.add(ambient);
 
-    const light = new THREE.DirectionalLight(0xffffff, 2);
-    light.position.set(0, 10, 2);
-    scene.add(light);
+    lights = {};
+    lights.spot = new THREE.SpotLight();
+    lights.spot.visible = true;
+    lights.spot.position.set(0, 20, 0);
+    lights.spotHelper = new THREE.SpotLightHelper(lights.spot);
+    lights.spotHelper.visible = false;
+    scene.add(lights.spotHelper);
+    scene.add(lights.spot);
 
+    params = {
+        spot: {
+            enable: false,
+            color: 0xffffff,
+            distance: 20,
+            angle: Math.PI/2,
+            penumbra: 0,
+            helper: false,
+            moving: false,
+        }
+    }
+
+    const gui = new dat.GUI ({autoPlace: false})
+    const guiContainer = document.getElementById('gui-container');
+    guiContainer.appendChild(gui.domElement);
+
+    guiContainer.style.position = 'fixed';
+
+    const spot = gui.addFolder('Spot');
+    spot.open();
+    spot.add(params.spot, 'enable').onChange(value => {
+        lights.spot.visible = value
+    });
+
+    spot.addColor(params.spot, 'color').onChange(value => lights.spot.color = new THREE.Color(value));
+    spot.add(params.spot, 'distance').min(0).max(20).onChange(value => lights.spot.distance = value);
+    spot.add(params.spot, 'angle').min(0.1).max(6.28).onChange(value => lights.spot.angle = value);
+    spot.add(params.spot, 'penumbra').min(0).max(1).onChange(value => lights.spot.penumbra = value);
+    spot.add(params.spot, 'helper').onChange(value => lights.spotHelper.visible = value);
+    spot.add(params.spot, 'moving');
+
+    //const light = new THREE.DirectionalLight(0xFFFFFF, 2);
+    //light.position.set(0, 10, 2);
+    //scene.add(light);
+
+    // Set up the renderer
+    const canvas = document.getElementById('threeContainer');
+    renderer = new THREE.WebGLRenderer({canvas: canvas});
+    renderer.setPixelRatio(window.devicePixelRatio);
+    resize();
+
+    // Add OrbitControls
     const controls = new THREE.OrbitControls(camera, renderer.domElement);
     controls.target.set(1, 2, 0);
     controls.update();
 
-    mode = 'open';
-    const btn = document.getElementById("btn");
-    btn.addEventListener('click', function() {
-       if (action.length === 2) {
-        if(mode === "open") {
-            action.forEach(action => {
-               action.timeScale = 1;
-            action.reset();  
-        action.play();         
-              });
-        }
-       }
+    const listener = new THREE.AudioListener();
+    camera.add(listener);
+
+    sound = new THREE.Audio(listener);
+    secondSound = new THREE.Audio(listener);
+
+    const audioLoader = new THREE.AudioLoader();
+    audioLoader.load('assets/audio/can_opening.mp3', function (buffer) {
+        sound.setBuffer(buffer);
+        sound.setLoop(false);
+        sound.setVolume(1.0);
     });
 
+    audioLoader.load('assets/audio/Can crush.mp3', function (buffer) {
+        secondSound.setBuffer(buffer);
+        secondSound.setLoop(false);
+        secondSound.setVolume(1.0);
+    });
+
+    // Button to control animations
+    mode = 'open';
+    const btn = document.getElementById("btn");
+    btn.addEventListener('click', function () {
+        if (actions.length === 2) {
+            if (mode === "open") {
+                actions.forEach(action => {
+                    action.timeScale = 1;
+                    action.reset();
+                    action.play();
+                    if(sound.isPlaying) sound.stop();
+                    sound.play();
+                });
+            }
+        }
+    });
+
+    const wireframeBtn = document.getElementById("toggleWireframe");
+    wireframeBtn.addEventListener('click', function() {
+        isWireFrame = !isWireFrame;
+        toggleWireframe(isWireFrame);
+    });
+
+    const rotateBtn = document.getElementById("rotate");
+    rotateBtn.addEventListener('click', function() {
+        if(loadedModel) {
+            const axis = new THREE.Vector3(0,1,0);
+            const angle = Math.PI / 8;
+            loadedModel.rotateOnAxis(axis, angle);
+        }
+        else {
+            console.warn("model not loaded yet");
+        }
+    });
+
+    const playSecondModelAnimationBtn = document.getElementById("playSecondModelAnimation");
+    playSecondModelAnimationBtn.addEventListener('click', function() {
+        if(secondModelActions.length > 0) {
+            secondModelActions.forEach(action => {
+                action.reset();
+                action.setLoop(THREE.LoopOnce);
+                action.clampWhenFinished = true;
+                action.play();
+                if(secondSound.isPlaying) sound.stop();
+                    secondSound.play();
+            });
+        } else {
+            console.warn("No animation available for the second model");
+        }
+    });
+
+    // Load the glTF model
     const loader = new THREE.GLTFLoader();
-    loader.load(assetPath + 'assets/models/soda_can_opening.glb', function(gltf) 
-    {
+
+    function loadModel(modelPath) {
+        if(loadedModel) {
+            scene.remove(loadedModel);
+        }
+    
+
+    loader.load(modelPath, function (gltf) {
         const model = gltf.scene;
+
+        model.position.set(0, 0, 0);
         scene.add(model);
 
+        loadedModel = model;
+
+        // Set up animations 
         mixer = new THREE.AnimationMixer(model);
         const animations = gltf.animations;
+        actions = [];
 
         animations.forEach(clip => {
             const action = mixer.clipAction(clip);
             actions.push(action);
         });
+
+        if(modelPath === 'assets/models/soda_can_crush.glb') {
+            secondMixer = mixer;
+            secondModelActions = actions;
+        }
     });
-
-    window.addEventListener('resize', onResize, false);
-
-    update();
 }
 
-function update() {
+loadModel('assets/models/soda_can_opening.glb');
+const switchBtn = document.getElementById("switchModel");
+switchBtn.addEventListener('click', function () {
+   loadModel('assets/models/soda_can_crush.glb')
+});
+
+
+    // Handle resizing
+    window.addEventListener('resize', resize, false);
+
+    animate();
+}
+
+
+function toggleWireframe(enable) {
+    scene.traverse(function (object) {
+        if(object.isMesh) {
+            object.material.wireframe = enable;
+        }
+    })
+}
+
+function animate() {
     requestAnimationFrame(animate);
 
+    // Update animations
     if (mixer) {
         mixer.update(clock.getDelta());
     }
 
+    if (secondModelMixer) {
+        secondModelMixer.update(clock.getDelta());
+    }
+
     renderer.render(scene, camera);
+
+    const time = clock.getElapsedTime();
+    const delta = Math.sin(time)*5;
+    if(params.spot.moving) {
+        lights.spot.position.x = delta;
+        lights.spotHelper.update();
+    }
 }
 
-function onResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
+function resize() {
+    const canvas = document.getElementById('threeContainer');
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    camera.aspect = width / height;
     camera.updateProjectionMatrix();
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(width, height);
 }
